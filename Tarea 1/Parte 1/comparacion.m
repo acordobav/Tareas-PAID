25ee399d6c3510936a6b73c7fd1ef9411be01aec
Arturo <arturocv16@gmail.com>
2020-05-24 18:56:37 -0600

    [Final] Tarea 1

--- Tarea 1/Parte 1/comparacion.m
+++ Tarea 1/Parte 1/comparacion.m
@@ -15,19 +15,13 @@ Y1 = im2double(Y1);
 Y2 = im2double(Y2);
 O = im2double(O);
 
-nr_p = norm(Y1(:, :, 1),'fro'); % Norma canal rojo promedio
-ng_p = norm(Y1(:, :, 2),'fro'); % Norma canal verde promedio
-nb_p = norm(Y1(:, :, 3),'fro'); % Norma canal azul promedio
+nr_p = norm(Y1(:, :, 1) - O(:, :, 1),'fro'); % Norma canal rojo promedio
+ng_p = norm(Y1(:, :, 2) - O(:, :, 2),'fro'); % Norma canal verde promedio
+nb_p = norm(Y1(:, :, 3) - O(:, :, 3),'fro'); % Norma canal azul promedio
 
-nr_b = norm(Y2(:, :, 1),'fro'); % Norma canal rojo bilineal
-ng_b = norm(Y2(:, :, 2),'fro'); % Norma canal verde bilineal
-nb_b = norm(Y2(:, :, 3),'fro'); % Norma canal azul bilineal
+nr_b = norm(Y2(:, :, 1) - O(:, :, 1),'fro'); % Norma canal rojo bilineal
+ng_b = norm(Y2(:, :, 2) - O(:, :, 2),'fro'); % Norma canal verde bilineal
+nb_b = norm(Y2(:, :, 3) - O(:, :, 3),'fro'); % Norma canal azul bilineal
 
-nr_o = norm(O(:, :, 1),'fro'); % Norma canal rojo original
-ng_o = norm(O(:, :, 2),'fro'); % Norma canal verde original
-nb_o = norm(O(:, :, 3),'fro'); % Norma canal azul original
-
-[nr_o nr_p nr_b]
-[ng_o ng_p ng_b]
-[nb_o nb_p nb_b]
-
+[nr_p ng_p ng_p]
+[nr_b ng_b ng_b]
