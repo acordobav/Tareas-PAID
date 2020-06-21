a91ff5ac95d0a9af9889272411465dde66cdb662
Arturo Cordoba <arturocv16@gmail.com>
2020-06-21 16:14:55 -0600

    Tarea 2 - Parte 1

---
+++ Tarea 2/Parte 1/marca_agua_1.m
@@ -0,0 +1,100 @@
+clc; clear; close all;
+pkg load signal;
+
+alpha = 0.1;
+
+% Lectura de la imagen original
+I = imread('imagen1.jpg');
+I = im2double(I);
+subplot(1,4,1);
+imshow(I);
+title('Imagen Original');
+
+% Lectura de la marca de agua
+W = imread('marca.jpg');
+W = im2double(W);
+subplot(1,4,2);
+imshow(W);
+title('Marca de agua');
+
+% ------------- Incrustacion de la marca de agua W en la imagen I
+
+% Se divide la imagen original en bloques de 8x8 y se les aplica la DCT-2D
+Z = zeros(8,8,1,4096); % Matriz para guardar cada bloque
+x = 1; % Contador para iterar sobre Z
+A = zeros(64,64); % Matriz para almacenar el valor DC de cada bloque
+for i = 1:64
+  i_fin = 8*i;
+  i_ini = i_fin - 7;
+  for j = 1:64
+    j_fin = j*8;
+    j_ini = j_fin-7;
+    block = I(i_ini:i_fin,j_ini:j_fin); % Se obtiene un bloque de 8x8
+    dct_block = dct2(block); % Se le aplica DCT-2D al bloque
+    Z(:,:,:,x) = dct_block;
+    A(i,j) = dct_block(1,1);
+    x = x+1;
+  endfor
+endfor
+
+% Calculo de la descomposicion en valores singulares de A
+[U,S,V] = svd(A);
+
+% Calculo de la SVD utilizando la matriz S y la marca de agua
+Aw_aux = S + (alpha * W);
+[U1,S1,V1] = svd(Aw_aux);
+
+% Calculo de Aw
+Aw = U * S1 * V';
+
+Iw = zeros(512, 512); % Matriz para guarda la imagen con la marca de agua
+x = 1; % Contador para iterar sobre Z
+for i = 1:64
+  i_fin = 8*i;
+  i_ini = i_fin - 7;
+  
+  for j = 1:64
+    j_fin = j*8;
+    j_ini = j_fin-7;
+    
+    F = Z(:,:,:,x); % Se obtiene el bloque
+    
+    % Cambio del valor DC de cada bloque por el valor correspondiente en Aw
+    F(1,1) = Aw(i,j);
+    
+    % Se calcula la DCT-2D inversa del bloque y se almacena en la nueva imagen
+    Iw(i_ini:i_fin,j_ini:j_fin) = idct2(F);
+    
+    x = x+1;
+  endfor
+endfor 
+subplot(1,4,3);
+imshow(Iw);
+title('Imagen modificada')
+
+
+% ------------- Extraccion de la marca de agua W2 de la imagen Iw
+
+% Se divide la imagen modificada en bloques de 8x8 y se les aplica
+% la DCT-2D para obtener el valor DC de cada bloque
+Aw = zeros(64,64);
+for i = 1:64
+  i_fin = 8*i;
+  i_ini = i_fin - 7;
+  for j = 1:64
+    j_fin = j*8;
+    j_ini = j_fin-7;
+    dct_block = dct2(Iw(i_ini:i_fin,j_ini:j_fin)); % DCT2 del bloque 8x8
+    Aw(i,j) = dct_block(1,1);    
+  endfor
+endfor
+
+% Descomposicion en valores singulares de Aw
+[Uw,Sw,Vw] = svd(Aw);
+
+D = U1 * Sw * V1';
+W2 = (1/alpha)*(D - S);
+
+subplot(1,4,4);
+imshow(W2);
+title('Marca de agua extraida');
---
+++ Tarea 2/Parte 1/marca_agua_2.m
@@ -0,0 +1,41 @@
+clc; clear; close all;
+pkg load signal;
+
+U1 = load('U1.mat').U1;
+V1 = load('V1.mat').V1;
+
+I = imread('imagen2.jpg');
+I = im2double(I);
+I_o = imread('imagen3.jpg');
+I_o = im2double(I_o);
+subplot(1,2,1);
+imshow(I);
+title('Imagen con ruido y marca de agua');
+
+% Se divide la imagen modificada en bloques de 4x4 y se les aplica
+% la DCT-2D para obtener el valor DC de cada bloque
+Aw = zeros(256,256);
+A = zeros(256,256);
+for i = 1:256
+  i_fin = 4*i;
+  i_ini = i_fin - 3;
+  for j = 1:256
+    j_fin = j*4;
+    j_ini = j_fin-3;
+    Aw(i,j) = dct2(I(i_ini:i_fin,j_ini:j_fin))(1,1);
+    A(i,j) = dct2(I_o(i_ini:i_fin,j_ini:j_fin))(1,1);
+  endfor
+endfor
+
+% Descomposicion en valores singulares de Aw y A
+[Uw,Sw,Vw] = svd(Aw);
+[U,S,V] = svd(A);
+
+% Reconstruccion de la marca de agua
+alpha = 0.1;
+D = U1 * Sw * V1';
+W = (1/alpha)*(D - S);
+
+subplot(1,2,2);
+imshow(W);
+title('Marca de agua extraida');
