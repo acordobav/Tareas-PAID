3ec24da1ccfb2f1137840cd632e6c743949bde18
Arturo Cordoba <arturocv16@gmail.com>
2020-07-19 00:44:17 -0600

    Parte 2 - Pregunta 3 - Tarea 3

---
+++ Tarea 3/Parte 2/inpainting.m
@@ -0,0 +1,17 @@
+function C = inpainting(A, B, kernel, num_iteration)
+  % A imagen que tiene que ser restaurada
+  % B mascara
+  % kernel de difusion
+  % num cantidad de iteraciones
+  [m,n] = size(B);
+  C = A;
+  [xp,yp] = find(B==255);
+  for iter = 1:num_iteration
+    for k = 1:length(xp)
+        i = xp(k); j = yp(k);
+        bloque = C(i-1:i+1,j-1:j+1);
+        C(i,j) = sum((bloque.*kernel)(:));
+    endfor
+  endfor
+endfunction
+
