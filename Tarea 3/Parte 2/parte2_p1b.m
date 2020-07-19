4123de16eed7e4d9ac284c71905081e285db340c
Arturo Cordoba <arturocv16@gmail.com>
2020-07-19 00:42:11 -0600

    Parte 2 - Pregunta 1 - Tarea 3

---
+++ Tarea 3/Parte 2/parte2_p1b.m
@@ -0,0 +1,23 @@
+clc; clear; close all;
+% Canal rojo de A
+Ar = [5 10 15 20;
+      4  8 12 16;
+      3  6  9 12;
+      2  4  6  8];
+% Canal verde de A  
+Ag = [1 2 3  4;
+      1 3 5  7;
+      2 4 6  8;
+      1 4 7 10];
+% Canal azul de A
+Ab = [ 0  0  0  0;
+       5  5  5  5;
+      10 10 10 10;
+      15 15 15 15];
+% Creacion de la matriz A
+A = cat(3, Ar, Ag, Ab);
+
+% Creacion de la matriz C
+C = zeros(size(A));
+C(A(:,:,:)<=10) = -30;
+C(A(:,:,:)>10) = 30;
