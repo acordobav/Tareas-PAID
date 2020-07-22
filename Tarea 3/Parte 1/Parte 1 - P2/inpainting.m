244f749a5caa12facad6657a78a6dd24423a18fc
Fagonar96 <fagonar96@gmail.com>
2020-07-22 17:19:03 -0600

    Funcion que realiza el inpainting

---
+++ Tarea 3/Parte 1/Parte 1 - P2/inpainting.m
@@ -0,0 +1,12 @@
+function C = inpainting(A, B, kernel, num_iteration)
+  % A: imagen que tiene que ser restaurada
+  % B: mascara con la region a restaurar
+  % kernel: kernel de difusion
+  % num_iteration: num cantidad de iteraciones
+  
+  B = logical(B);
+  for iter = 1:num_iteration
+    C = conv2(A, kernel,'same');
+    A(B) = C(B);
+  endfor
+endfunction
